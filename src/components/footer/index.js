5bc685e122a7ff9b61f31bcab0b5b8f83b1489dd
Murat Umutlu <murat@muum.dev>
2021-02-14 02:58:13 +0300

    footer changed

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { FiGithub } from "react-icons/fi";
 
 import "./index.css";
 
@@ -6,9 +7,15 @@ export default class index extends Component {
   render() {
     return (
       <footer className="footer">
-        <div className="container-fluid">
-          <span className="text-muted">MUUM.DEV TRADER VIEW</span>
-          <span className="text-muted text-right">V.1.0</span>
+        <div className="container">
+          <span className="text-muted">Developed by muumdev</span>
+          <a
+            alt="github repo"
+            className="text-muted text-right"
+            href="https://github.com/muum/reactjs-draggable-fintech-dashboard"
+          >
+            <FiGithub size={24} />
+          </a>
         </div>
       </footer>
     );
